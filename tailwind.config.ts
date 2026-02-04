0bb1b43f3550bd8d36d5766c46078e373cb02c74
Thiago Moraes <tmr@icomp.ufam.edu.br>
2026-02-04 18:37:33 -0400

    feat: add new exports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
-import type { Config } from "tailwindcss";
-
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  corePlugins: {
+    preflight: false,
   },
-  plugins: [],
-} satisfies Config;
+};
